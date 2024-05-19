853c61be868ee5e6d19a9d7fcb4a379df2b6c4b8
Werter Pires <werterpires23@hotmail.com>
2024-05-19 16:19:49 -0300

    add User Repository and User classes

---
+++ src/manager/controllers/copy/UserController.java
@@ -0,0 +1,5 @@
+package manager.controllers.copy;
+
+public class UserController {
+
+}
--- src/manager/persistence/UserRepository.java
+++ src/manager/persistence/UserRepository.java
@@ -1,5 +1,69 @@
 package manager.persistence;
 
-public class UserRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+import manager.entities.User;
+
+@ManagedBean
+@ApplicationScoped
+public class UserRepository {
+	private List<User> users = new ArrayList<>();
+
+	public UserRepository() {
+		users.add(new User(1, "Beanderson Carlos", "beanderson@me.com", "123456", 1));
+		users.add(new User(2, "Joaquim Tavates", "joaquim@me.com", "123456", 1));
+		users.add(new User(3, "Maria Silva", "maria@me.com", "123456", 2));
+		users.add(new User(4, "Ana Pereira", "ana@me.com", "123456", 3));
+		users.add(new User(5, "Carlos Souza", "carlos@me.com", "123456", 2));
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public User getUser(int id) {
+		for (User user : users) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public User getUser(String email) {
+		for (User user : users) {
+			if (user.getEmail().equals(email)) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public User addUser(User user) {
+		users.add(user);
+		return user;
+	}
+
+	public User updateUser(User user) {
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getId() == user.getId()) {
+				users.set(i, user);
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public User deleteUser(int id) {
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getId() == id) {
+				return users.remove(i);
+			}
+		}
+		return null;
+	}
 }
